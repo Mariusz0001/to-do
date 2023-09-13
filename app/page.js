fc8d96b21360efdf81a92d39c3b7a56dcd3cb8c7
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-09-13 13:32:45 +0200

    feature added task list component

--- app/page.js
+++ app/page.js
@@ -2,65 +2,14 @@
 import Logo from "@/components/logo";
 import styles from "./styles/page.module.css";
 import Layout from "@/components/layout";
+import TasksList from "@/components/tasks-list";
 
 export default function Home() {
   return (
     <Layout>
        <Logo></Logo>
       <main className={styles.main}>
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <TasksList></TasksList>
       </main>
     </Layout>
   );
---
+++ components/tasks-list.js
@@ -0,0 +1,9 @@
+import styles from "../app/styles/tasks_list.module.css";
+
+export default function TasksList(children){
+    return <>
+        <div className={styles.card}>
+            
+        </div>
+    </>;
+}
