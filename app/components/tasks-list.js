7d64b48d549d2f9e7c591872d6b46e72b01a9e89
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-09-14 10:02:10 +0200

    added ui library shadcn + buttons

--- app/components/tasks-list.js
+++ app/components/tasks-list.js
@@ -1,9 +1,31 @@
 import styles from "../styles/tasks_list.module.css";
+import { Button } from "@/app/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
 
-export default function TasksList(children){
-    return <>
-        <div className={styles.card}>
-            
-        </div>
-    </>;
+export default function TasksList(children) {
+  return (
+    <>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Todo</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">
+            +Add task
+          </Button>
+        </CardFooter>
+      </Card>
+      <div className={styles.card}></div>
+    </>
+  );
 }
