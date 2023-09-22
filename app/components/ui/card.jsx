f9af078df2ed7475f576315ecb71efd9790da6bb
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-09-22 22:25:27 +0200

    added dark mode with themes

--- app/components/ui/card.jsx
+++ app/components/ui/card.jsx
@@ -4,7 +4,7 @@ import { cn } from "@/app/lib/utils";
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
+    className={cn("bg-white dark:bg-slate-800 rounded-lg border text-card-foreground shadow-sm", className)}
     {...props} />
 ))
 Card.displayName = "Card"
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,27 @@
-import './styles/globals.css'
-import { Inter } from 'next/font/google'
+import "./styles/globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "./components/theme-provider";
+import { ThemeSwitcher } from "./components/theme-switcher";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'To Do App',
-  description: 'A ToDo application is a digital tool designed to help users organize and manage tasks and activities.',
-}
+  title: "To Do App",
+  description:
+    "A ToDo application is a digital tool designed to help users organize and manage tasks and activities.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-slate-50 dark:bg-[#0d1117] duration-200`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeSwitcher />
+          <main>{children}</main>
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
