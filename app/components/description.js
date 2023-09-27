c723cabc6943374e09eba3960f6cefaee732c3be
Mariusz0001 <mariusz.pawlus95@gmail.com>
2023-09-27 22:01:43 +0200

    fix - description formatting

--- app/components/description.js
+++ app/components/description.js
@@ -7,20 +7,33 @@ export default function Description({ ...props }) {
     >
       <li className="font-semibold text-s">
         Are you looking for a seamless and efficient way to manage your tasks
-        and stay organized? Look no further! <a class="underline decoration-sky-500/30">Our TodoApp</a> is here to simplify
-        your life and boost your productivity!
-      </li><br></br>
-      <li className="font-bold text-xl">🌟 Why Choose Our <a class="underline decoration-sky-500/30">Our TodoApp</a>? 🌟
-      </li><br></br>
-      <li className="font-bold text-xl">User-Friendly Interface:</li><li>
-      Our app is designed with you in mind. The intuitive interface ensures that
-      anyone can start using it immediately, without a steep learning curve.</li><br></br>
-      <li className="font-bold">Convenience at Your Fingertips:</li> <li>Access your
-      to-do list from anywhere, at any time, on any device. Whether you're at
-      home, in the office, or on the go, you'll always be in control of your
-      tasks.</li><br></br>
-      <li className="font-bold">Effortless Task Management:</li><li> Easily create,
-      edit, and prioritize tasks.</li><br></br>
+        and stay organized? Look no further!{" "}
+        <a class="underline decoration-sky-500/30">Our TodoApp</a> is here to
+        simplify your life and boost your productivity!
+      </li>
+      <br></br>
+      <li className="font-bold text-xl">
+        🌟 Why Choose Our{" "}
+        <a class="underline decoration-sky-500/30">Our TodoApp</a>? 🌟
+      </li>
+      <br></br>
+      <li className="font-bold text-xl">User-Friendly Interface:</li>
+      <li>
+        Our app is designed with you in mind. The intuitive interface ensures
+        that anyone can start using it immediately, without a steep learning
+        curve.
+      </li>
+      <br></br>
+      <li className="font-bold">Convenience at Your Fingertips:</li>{" "}
+      <li>
+        Access your to-do list from anywhere, at any time, on any device.
+        Whether you're at home, in the office, or on the go, you'll always be in
+        control of your tasks.
+      </li>
+      <br></br>
+      <li className="font-bold">Effortless Task Management:</li>
+      <li> Easily create, edit, and prioritize tasks.</li>
+      <br></br>
       <li className="font-bold">Reminders and Notifications:</li> Stay on top of
       your tasks with timely reminders and notifications. Never miss an
       important deadline again!
