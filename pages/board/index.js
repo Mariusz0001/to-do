62970c87f07f3f240eee362fef0ca4f63936b9cf
Mariusz0001 <mariusz.pawlus95@gmail.com>
2023-10-14 18:16:17 +0200

    cleaning tests changes

--- pages/board/index.js
+++ pages/board/index.js
@@ -1,13 +1,9 @@
 import TasksList from "@/app/components/tasks/tasks-list";
-import Dnd from "@/app/components/dnd";
 
 export default function BoardPage() {
   return (
-    <>
-      <Dnd></Dnd>
-      <div className="flex justify-center px-2 py-10">
-        <TasksList></TasksList>
-      </div>
-    </>
+    <div className="flex justify-center px-2 py-10">
+      <TasksList></TasksList>
+    </div>
   );
 }
