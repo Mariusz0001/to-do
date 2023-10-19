f95b8337115605b9cc553c5d774055b65f3e765c
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-10-19 22:17:11 +0200

    fix - checkbox isDone

--- app/components/ui/todoapp/task.jsx
+++ app/components/ui/todoapp/task.jsx
@@ -7,14 +7,12 @@ import { EditableTask } from "./editableTask";
 
 const Task = React.forwardRef(({ className, ...props }, ref) => {
   const checkboxRef = useRef(null);
-  const taskRef = useRef(null);
 
   const handleClickCheckbox = async () => {
     if (props.id && props.id.length) await props.handleaccomplishtask(props.id);
   };
 
-  const handleEditTask = (id, taskName) => 
-    props.handleEditTask(id, taskName);  
+  const handleEditTask = (id, taskName) => props.handleEditTask(id, taskName);
 
   const isTaskDone = () =>
     props.status != BOARD_TYPE[0].value && props.status != BOARD_TYPE[1].value;
@@ -24,20 +22,26 @@ const Task = React.forwardRef(({ className, ...props }, ref) => {
       <div
         className={
           isTaskDone()
-            ? "line-through"
+            ? "line-through drop-shadow-lg flex items-center space-x-3"
             : "cursor-pointer drop-shadow-lg flex items-center space-x-3"
         }
       >
-        {!isTaskDone() && (
-          <Checkbox
-            id="taskCheckbox"
-            className="default:ring-2 w-5 h-5 dark:bg-zinc-900 dark:border-zinc-800 dark:hover:bg-zinc-700"
-            ref={checkboxRef}
-            checked={isTaskDone()}
-            onClick={handleClickCheckbox}
-          ></Checkbox>
-        )}
-        <EditableTask id={props.id} handleEditTask={handleEditTask} readOnly={isTaskDone()}>{props.children}</EditableTask>
+        <Checkbox
+          id="taskCheckbox"
+          className="default:ring-2 w-5 h-5 dark:bg-zinc-900 dark:border-zinc-800 dark:hover:bg-zinc-700"
+          ref={checkboxRef}
+          checked={isTaskDone()}
+          disabled={isTaskDone()}
+          onClick={handleClickCheckbox}
+        ></Checkbox>
+
+        <EditableTask
+          id={props.id}
+          handleEditTask={handleEditTask}
+          readOnly={isTaskDone()}
+        >
+          {props.children}
+        </EditableTask>
       </div>
     </div>
   );
