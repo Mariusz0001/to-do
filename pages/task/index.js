57a39d2290d70ee607b081a2c6ba2550d33197a0
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-10-27 14:49:55 +0200

    added getting task details with axios

--- pages/task/index.js
+++ pages/task/index.js
@@ -1,42 +1,51 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import useSWR from "swr";
 import { getUserToken } from "@/app/lib/utils";
 import { useEffect, useState } from "react";
+import { Input } from "@/app/components/ui/input";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function TaskDetails() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
-
-  const [mounted, setMounted] = useState(false);
+  const router = useRouter();
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    setMounted(true);
-  }, []);
+    let id = router.query.id;
+    if (id && id.length) {
+      axios
+        .get(
+          process.env.NEXT_PUBLIC_BACKEND_URL +
+            process.env.NEXT_PUBLIC_PERSONALTASKS_URL +
+            `/byId/${id}`,
+          { headers: { Authorization: `Bearer ${getUserToken()}` } }
+        )
+        .then((response) => {
+          setData(response.data);
+        });
+    }
+  }, [router.isReady]);
 
-  const headers = {
-    Authorization: `Bearer ${getUserToken()}`,
-  };
+  if (!data) return <>Loading..</>;
 
-  const fetcher = (url) =>
-    fetch(url, {
-      method: "GET",
-      headers: headers,
-    }).then((res) => res.json());
-
-  const { data, error, isLoading } = useSWR(
-    mounted && id
-      ? process.env.NEXT_PUBLIC_BACKEND_URL +
-          process.env.NEXT_PUBLIC_PERSONALTASKS_URL +
-          `/byId/${id}`
-      : null,
-    fetcher
+  return (
+    <div className="p-4">
+      <div className="mb-6">
+        <Input
+          type="name"
+          placeholder="Name"
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+          className="dark:bg-zinc-900 w-full p-2 mb-4 border rounded focus:outline-none"
+        />
+      </div>
+      <Input
+        type="name"
+        placeholder="Status"
+        value={data.status}
+        onChange={(e) => setData({ ...data, status: e.target.value })}
+        className="dark:bg-zinc-900 w-full p-2 mb-4 border rounded focus:outline-none"
+      />
+    </div>
   );
-
-  if (error) return <h3>Failed to fetch data</h3>;
-
-  if (isLoading) return <h3>Loading data...</h3>;
-
-  return <div className="p-4">id:{data && data.id}</div>;
 }
