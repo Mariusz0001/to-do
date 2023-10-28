685f63be6337cc93322c078a2ccde15199e81719
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-10-28 20:48:06 +0200

    added form task details

---
+++ app/lib/enums/priority.js
@@ -0,0 +1,7 @@
+export const PRIORITY = [
+    { value: "HIGHEST", text: "Highest" },
+    { value: "HIGH", text: "High" },
+    { value: "MEDIUM", text: "Medium" },
+    { value: "LOW", text: "Low" },
+    { value: "LOWEST", text: "Lowest" }
+];
--- pages/task/index.js
+++ pages/task/index.js
@@ -5,6 +5,20 @@ import { useEffect, useState } from "react";
 import { Input } from "@/app/components/ui/input";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { Textarea } from "@/app/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/components/ui/select";
+import { BOARD_TYPE } from "@/app/lib/enums/boardType";
+import Board from "@/app/components/tasks/board";
+import { Button } from "@/app/components/ui/button";
+import { PRIORITY } from "@/app/lib/enums/priority";
 
 export default function TaskDetails() {
   const router = useRouter();
@@ -28,24 +42,131 @@ export default function TaskDetails() {
 
   if (!data) return <>Loading..</>;
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
   return (
-    <div className="p-4">
-      <div className="mb-6">
-        <Input
-          type="name"
-          placeholder="Name"
-          value={data.name}
-          onChange={(e) => setData({ ...data, name: e.target.value })}
-          className="dark:bg-zinc-900 w-full p-2 mb-4 border rounded focus:outline-none"
-        />
+    <form onSubmit={handleSubmit}>
+      <div className="lg:flex lg:flex-nowrap p-2">
+        <div className="lg:basis-1/2 lg:mx-60 p-2">
+          <div className="mb-4 lg:w-3/4">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Task Name
+            </label>
+            <Input
+              id="name"
+              name="name"
+              value={data.name}
+              onChange={(e) => setData({ ...data, name: e.target.value })}
+              placeholder="Enter task name"
+            />
+          </div>
+
+          <div className="mb-4 lg:w-3/4">
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Task Description
+            </label>
+            <Textarea
+              id="description"
+              name="description"
+              value={data.description}
+              onChange={(e) =>
+                setData({ ...data, description: e.target.value })
+              }
+              placeholder="Enter task description"
+            />
+          </div>
+
+          <div className="mb-4 lg:w-3/4">
+            <label
+              htmlFor="status"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Status
+            </label>
+            <Select
+              onValueChange={(value) => setData({ ...data, status: value })}
+              defaultValue={data.status}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Status" />
+              </SelectTrigger>
+              <SelectContent>
+                {BOARD_TYPE.map((type, index) => (
+                  <SelectItem key={index} value={type.value}>
+                    {type.text}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="mb-4 lg:w-3/4">
+            <label
+              htmlFor="priority"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Priority
+            </label>
+            <Select
+              onValueChange={(value) => setData({ ...data, status: value })}
+              defaultValue={data.status}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Priority" />
+              </SelectTrigger>
+              <SelectContent>
+                {PRIORITY.map((type, index) => (
+                  <SelectItem key={index} value={type.value}>
+                    {type.text}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className="lg:basis-1/2 mt-8 p-2">
+          <div className="mb-4 lg:w-3/4">
+            <label
+              htmlFor="creationDate"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Creation date
+            </label>
+            <Input
+              id="creationDate"
+              name="creationDate"
+              value={data.creationDate}
+              disabled
+              placeholder="2020.01.01"
+            />
+          </div>
+          <div className="mb-4 lg:w-3/4">
+            <label
+              htmlFor="createdBy"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Created by
+            </label>
+            <Input
+              id="createdBy"
+              name="createdBy"
+              value={data.createdBy}
+              disabled
+            />
+          </div>
+        </div>
       </div>
-      <Input
-        type="name"
-        placeholder="Status"
-        value={data.status}
-        onChange={(e) => setData({ ...data, status: e.target.value })}
-        className="dark:bg-zinc-900 w-full p-2 mb-4 border rounded focus:outline-none"
-      />
-    </div>
+      <div className="flex p-10 justify-center">
+        <Button type="submit" className="w-[300px]">Submit</Button>
+      </div>
+    </form>
   );
 }
