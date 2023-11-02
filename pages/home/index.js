f50ace2b4cdb6a3a8d983a2f475173963eda230b
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-11-02 11:42:31 +0100

    added getTasksCountByStatus

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,20 +1,43 @@
-"use client";
 import Logo from "@/app/components/logo";
 import { useAuth } from "@/app/lib/authProvider";
+import { getUserToken } from "@/app/lib/utils";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import useSWR from "swr";
+import { BOARD_TYPE } from "@/app/lib/enums/boardType";
 
 const UserDashboard = () => {
   const { isLoggedIn } = useAuth();
   const router = useRouter();
 
+  const headers = {
+    Authorization: `Bearer ${getUserToken()}`,
+  };
+  const fetcher = (url) =>
+    fetch(url, {
+      method: "GET",
+      headers: headers,
+    }).then((res) => res.json());
+
+  const { isLoading, data } = useSWR(
+    process.env.NEXT_PUBLIC_BACKEND_URL +
+      process.env.NEXT_PUBLIC_PERSONALTASKS_URL + '/userStats',
+    fetcher
+  );
+
   useEffect(() => {
     if (!isLoggedIn) router.push("/");
   }, []);
 
-  const tasksDone = 5; // todo Replace with the actual number of tasks done
-  const tasksRemaining = 10; // todo Replace with the actual number of tasks remaining
+  if(isLoading) return <>Loading...</>;
 
+  const getTasksCountByStatus = (status) => {
+    const items = data?.find(item => item.status === status);
+    return items ? items.count : 0;
+  };
+  
+  const tasksDone = getTasksCountByStatus(BOARD_TYPE[2].value);
+  const tasksRemaining = getTasksCountByStatus(BOARD_TYPE[0].value) + getTasksCountByStatus(BOARD_TYPE[1].value);;
   return (
     <div className="lg:space-x-10 mb-4 lg:p-20 break-words">
       <div className="mt-10 p-6 w-[90vw] h-[50vh]">
@@ -25,18 +48,21 @@ const UserDashboard = () => {
           We greatly appreciate your choice to use Todoapp.
         </div>
         Your productivity matters to us!
-        <div className="mt-4 flex flex-shrink space-y-10">
-          <div className="text-xl font-semibold">
-            Tasks Completed 🎉{" "}
-            <div className="text-3xl text-green-500">{tasksDone}</div>
-          </div>
-          <div className="text-xl font-semibold">
-            Tasks Remaining 🔥{" "}
-            <div className="text-3xl align-middle text-yellow-500">
-              {tasksRemaining}
+
+        {data && data.length && 
+          <div className="mt-4 flex flex-shrink space-y-10">
+            <div className="text-xl font-semibold">
+              Tasks Completed 🎉{" "}
+              <div className="text-3xl text-green-500">{tasksDone}</div>
+            </div>
+            <div className="text-xl font-semibold">
+              Tasks Remaining 🔥{" "}
+              <div className="text-3xl align-middle text-yellow-500">
+                {tasksRemaining}
+              </div>
             </div>
           </div>
-        </div>
+        }
       </div>
       <Logo></Logo>
     </div>
