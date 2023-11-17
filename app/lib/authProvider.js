7e9f36ee83afb428025969009a02d592e76c4425
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-11-17 14:30:51 +0100

    added loging with google

--- app/lib/authProvider.js
+++ app/lib/authProvider.js
@@ -5,36 +5,42 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setTokenState] = useState(getUserToken() || "");
-  const [userName, setUserNameState] = useState(getUserName() || '');
+  const [userName, setUserNameState] = useState(getUserName() || "");
 
-  const setUserAuthAndState = (token, userName) => {    
+  const setUserAuthAndState = (token, userName) => {
     setUserToken(token);
     setUserName(userName);
 
     setTokenState(token);
     setUserNameState(userName);
-  }
+  };
 
   const login = (newToken) => {
-    let claims = parseJwt(newToken);
-    setUserAuthAndState(newToken, claims.Name);
+    if (newToken && newToken.length > 0) {
+      let claims = parseJwt(newToken);
+      setUserAuthAndState(newToken, claims.Name);
+    }
   };
 
   const logout = () => {
-    setUserAuthAndState('', '');
+    setUserAuthAndState("", "");
   };
 
   function parseJwt(token) {
-    if (!token) { return; }
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    if (!token) {
+      return;
+    }
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace("-", "+").replace("_", "/");
     return JSON.parse(window.atob(base64));
-}
+  }
 
   const isLoggedIn = !!token;
 
   return (
-    <AuthContext.Provider value={{ token, login, logout, isLoggedIn, userName }}>
+    <AuthContext.Provider
+      value={{ token, login, logout, isLoggedIn, userName }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- pages/login/index.js
+++ pages/login/index.js
@@ -5,18 +5,20 @@ import { authenticate } from "@/app/lib/commands/authenticate";
 import { useRouter } from "next/router";
 import { useAuth } from "@/app/lib/authProvider";
 import NavigationLink from "@/app/components/ui/todoapp/navigationLink";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { LOGIN_PROVIDER_TYPE } from "@/app/lib/enums/loginProviderType";
+import { authenticateWithProvider } from "@/app/lib/commands/authenticateWithProvider";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
   const { login, isLoggedIn } = useAuth();
 
-  if(isLoggedIn)
-    router.push("/home");
+  if (isLoggedIn) router.push("/home");
 
   const handleLogin = async () => {
     try {
@@ -27,12 +29,11 @@ const Login = () => {
         login(result.token);
         router.back();
       } else {
-        setError(true);
+        setError("Invalid username or password");
       }
     } catch {
-      setError(true);
-    }
-    finally{
+      setError("Invalid username or password");
+    } finally {
       setIsLoading(false);
     }
   };
@@ -41,7 +42,7 @@ const Login = () => {
     <div className="flex justify-center items-center min-h-screen">
       <div className="bg-white dark:bg-zinc-800 p-8 rounded-lg shadow-md w-full max-w-md space-y-5">
         <h1 className="text-2xl font-semibold mb-4">Login</h1>
-        {error && <p className="text-red-400">Invalid username or password</p>}
+        {error && <p className="text-red-400">{error}</p>}
         <Input
           type="text"
           placeholder="Username"
@@ -56,13 +57,42 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           className="dark:bg-zinc-900 w-full p-2 mb-6 border rounded focus:outline-none"
         />
-        <Button onClick={handleLogin} className="w-full p-2" isLoading={isLoading}
+        <Button
+          onClick={handleLogin}
+          className="w-full p-2"
+          isLoading={isLoading}
         >
           Login
         </Button>
         <div className="w-full justify-center items-center">
           <NavigationLink url="/signup">or, sign up</NavigationLink>
         </div>
+
+        <GoogleOAuthProvider clientId="989615753316-q92qkqpc79979a01daib0virp5k5c5ag.apps.googleusercontent.com">
+          <GoogleLogin
+            onSuccess={async (credentialResponse) => {
+              try {
+                setIsLoading(true);
+                let token = await authenticateWithProvider(
+                  LOGIN_PROVIDER_TYPE.GOOGLE,
+                  credentialResponse.credential
+                );
+                if (token) {
+                  await login(token);
+                  router.back();
+                } else setError("Login failed");
+              } catch (e) {
+                console.log(e);
+                setError("Login failed");
+              } finally {
+                setIsLoading(false);
+              }
+            }}
+            onError={() => {
+              setError("Login failed");
+            }}
+          />
+        </GoogleOAuthProvider>
       </div>
     </div>
   );
