2d918815e22e996ee1327df8e262ac3115876a92
Mariusz <mariusz.pawlus95@gmail.com>
2024-11-26 16:19:44 +0100

    Feature/textarea tooltip (#31) (#32)
    * feat tooltip title
    
    * feat disabled textarea resizing
    
    * feat refactor editableTask

--- app/components/ui/textarea.jsx
+++ app/components/ui/textarea.jsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import styles from "../../styles/task.module.css"
 
 import { cn } from "@/app/lib/utils"
 
@@ -7,12 +8,15 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
     (<textarea
       className={cn(
         "overscroll-none flex min-h-[60px] w-full rounded-md border-input bg-transparent px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        styles.textareas,
         className
       )}
       ref={ref}
+      title={props && props.value ? props.value : ""}
       {...props} />)
   );
 })
 Textarea.displayName = "Textarea"
+
 
 export { Textarea }
