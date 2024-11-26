a8815b076bc2742f219c367f5094c1ea04339c82
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2024-11-26 15:56:01 +0100

    feat disabled textarea resizing

--- app/components/ui/textarea.jsx
+++ app/components/ui/textarea.jsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import styles from "../../styles/task.module.css"
 
 import { cn } from "@/app/lib/utils"
 
@@ -7,6 +8,7 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
     (<textarea
       className={cn(
         "overscroll-none flex min-h-[60px] w-full rounded-md border-input bg-transparent px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        styles.textareas,
         className
       )}
       ref={ref}
@@ -15,5 +17,6 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   );
 })
 Textarea.displayName = "Textarea"
+
 
 export { Textarea }
